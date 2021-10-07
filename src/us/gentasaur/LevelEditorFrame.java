79b0fd0186f4c6aa9a8b2ec08f992b0778aaee0f
gsemaj <gentsemaj@live.com>
2021-10-06 21:37:39 -0400

    Add level implementation

--- src/us/gentasaur/LevelEditorFrame.java
+++ src/us/gentasaur/LevelEditorFrame.java
@@ -1,10 +1,17 @@
 package us.gentasaur;
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class LevelEditorFrame extends JFrame {
+	
+	private ArrayList<Byte[]> level;
+	private int levelX, levelY, levelW, levelH;
+	
 	public LevelEditorFrame() {
 		super("LevelEditor");
+		level = null;
 		
 		this.setSize(800, 600);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -14,5 +21,29 @@ public class LevelEditorFrame extends JFrame {
 		this.setLayout(null);
 		
 		this.setVisible(true);
+	}
+	
+	public void initLevel(int x, int y, int w, int h) {
+		levelX = x;
+		levelY = y;
+		levelW = w;
+		levelH = h;
+		
+		level = new ArrayList<Byte[]>();
+		clearLevel();
+	}
+	
+	public void clearLevel() {
+		level.clear();
+		level.add(LevelEditorFrame.getEmptyFloor(levelW, levelH));
+	}
+	
+	public static Byte[] getEmptyFloor(int w, int h) {
+		Byte[] bytes = new Byte[w * h];
+		byte empty = (byte) 0x00;
+		for(int i = 0; i < w * h; i++) {
+			bytes[i] = empty;
+		}
+		return bytes;
 	}
 }
